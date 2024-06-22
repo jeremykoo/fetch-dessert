a1ca805d756485782fbbd10b9a29d9047afb5f21
jeremykoo <38170465+jeremykoo@users.noreply.github.com>
2024-06-22 16:04:18 -0700

    created xcode project

---
+++ Fetch Dessert/Fetch_DessertApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_DessertApp.swift
+//  Fetch Dessert
+//
+//  Created by Jeremy Koo on 6/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_DessertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
